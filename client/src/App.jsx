53ba63b15ddd2eed282b6e38cda2fdad6904d61d
Pratyush Thapa <pratyushthapa2004@gmail.com>
2026-06-08 01:42:07 +0530

    Set API URL to localhost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://personal-task-manager-api-zqhg.onrender.com";
+const API_URL = "http://localhost:5000";
 
 function App() {
   const [tasks, setTasks] = useState([]);
