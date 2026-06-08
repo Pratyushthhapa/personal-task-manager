1d54e9d7b7e3dd8a77e7952530ad1fdb47be807b
Pratyush Thapa <pratyushthapa2004@gmail.com>
2026-06-08 16:41:54 +0530

    Fix API URL for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://personal-task-manager-api-zqhg.onrender.com";
 
 function App() {
   const [tasks, setTasks] = useState([]);
